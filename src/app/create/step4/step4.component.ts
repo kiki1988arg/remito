7d59acc4333cd1682503dce1f1e9332924333043
kiki1988arg <ezequielarakaki@hotmail.com>
2019-01-22 16:29:36 -0300

    d&d

--- src/app/create/step4/step4.component.ts
+++ src/app/create/step4/step4.component.ts
@@ -11,48 +11,33 @@ export class Step4Component implements OnInit {
 
   group: FormGroup;
 
-
-  // todo = [
-  //   'Get to work',
-  //   'Pick up groceries',
-  //   'Go home',
-  //   'Fall asleep'
-  // ];
-
-  // done = [
-  //   'Get up',
-  //   'Brush teeth',
-  //   'Take a shower',
-  //   'Check e-mail',
-  //   'Walk dog'
-  // ];
   todo = [
 
-    { title: '1' },
-    { title: '2' },
-    { title: '3' },
-    { title: '4' }
+    { title: '1', aux: 'jeje' },
+    { title: '2', aux: 'jeje' },
+    { title: '3', aux: 'jeje' },
+    { title: '4', aux: 'jeje' }
 
   ];
   done = [
     [
-      { title: 'Get to work' },
-      { title: 'Pick up groceries' },
-      { title: 'Go home' },
-      { title: 'Fall asleep' }
+      { title: 'Get to work', aux: 'jeje' },
+      { title: 'Pick up groceries', aux: 'jeje' },
+      { title: 'Go home', aux: 'jeje' },
+      { title: 'Fall asleep', aux: 'jeje' }
     ],
     [
-      { title: 'Get up' },
-      { title: 'Brush teeth' },
-      { title: 'Take a shower' },
-      { title: 'Check e-mail' },
-      { title: 'Walk dog' }
+      { title: 'Get up', aux: 'jeje' },
+      { title: 'Brush teeth', aux: 'jeje' },
+      { title: 'Take a shower', aux: 'jeje' },
+      { title: 'Check e-mail', aux: 'jeje' },
+      { title: 'Walk dog', aux: 'jeje' }
     ],
     [
-      { title: 'Get to work' },
-      { title: 'Pick up groceries' },
-      { title: 'Go home' },
-      { title: 'Fall asleep' }
+      { title: 'Get to work', aux: 'jeje' },
+      { title: 'Pick up groceries', aux: 'jeje' },
+      { title: 'Go home', aux: 'jeje' },
+      { title: 'Fall asleep', aux: 'jeje' }
     ]
   ];
 
